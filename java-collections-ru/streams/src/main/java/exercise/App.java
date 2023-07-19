cd60f186c77264eeff9e7485d275e4fdb5e76e22
@hexlet/cli <support@hexlet.io>
2023-07-19 17:48:39 +0300

    submit java-collections-ru/streams

--- java-collections-ru/streams/src/main/java/exercise/App.java
+++ java-collections-ru/streams/src/main/java/exercise/App.java
@@ -1,8 +1,31 @@
 package exercise;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Arrays;
 
 // BEGIN
-
+class App {
+    public static void main(String[] args) {
+        String[] emails = {
+            "info@gmail.com",
+            "info@yandex.ru",
+            "mk@host.com",
+            "support@hexlet.io",
+            "info@hotmail.com",
+            "support.yandex.ru@host.com"
+        };
+        List<String> listOfAddresses = new ArrayList<>(List.of(emails));
+        System.out.println(getCountOfFreeEmails(listOfAddresses));
+    }
+    public static long getCountOfFreeEmails(List<String> addresses) {
+        List<String> freeDomains = new ArrayList<>();
+        freeDomains.add("gmail.com");
+        freeDomains.add("yandex.ru");
+        freeDomains.add("hotmail.com");
+        return addresses.stream()
+                .map(str -> str.substring(str.indexOf('@') + 1))
+                .filter(str -> freeDomains.contains(str))
+                .count();
+    }
+}
 // END

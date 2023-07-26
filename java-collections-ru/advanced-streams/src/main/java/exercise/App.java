3ad5b9495443c34b50d7198b78ff8bb5364a9ba0
Boris Sturua <stboris65@gmail.com>
2023-07-26 16:24:24 +0300

    additional gradle files

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -2,7 +2,21 @@ package exercise;
 
 import java.util.stream.Collectors;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 // BEGIN
-
+class App {
+    public static void main(String[] args) {
+        String test = "[foo:bar]environment=\"X_FORWARDED_var1=111\"[bar:baz]";
+        System.out.println(getForwardedVariables(test));
+    }
+    public static String getForwardedVariables(String content) {
+        return Arrays.stream(content.split("envi"))
+                .filter(s -> s.startsWith("ronment="))
+                .flatMap(line -> Stream.of(line.split("X_FOR")))
+                .filter(s -> s.startsWith("WARDED_"))
+                .map(s -> s.substring(7, s.indexOf(s.contains(",") ? "," : "\"")))
+                .collect(Collectors.joining(","));
+    }
+}
 //END

98b17e76c1de8db973c0cb053bfb45a3b8432b6b
@hexlet/cli <support@hexlet.io>
2023-07-28 18:30:05 +0300

    submit java-collections-ru/other-collections

--- java-collections-ru/other-collections/src/main/java/exercise/App.java
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -1,21 +1,39 @@
 package exercise;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.TreeMap;
 
 // BEGIN
 class App {
     public static void main(String[] args) {
-    Map<String, Object> data1 = new HashMap<>(Map.of("one", "eon", "two", "two", "four", true));
-    System.out.println(data1); //=> {two=two, four=true, one=eon}
-    Map<String, Object> data2 = new HashMap<>(Map.of("two", "own", "zero", 4, "four", true));
-    System.out.println(data2); //=> {zero=4, two=own, four=true}
-
-    Map<String, String> result = App.genDiff(data1, data2);
-    System.out.println(result); //=> {four=unchanged, one=deleted, two=changed, zero=added}
+        Map<String, Object> data1 = new HashMap<>(Map.of("one", "eon", "two", "two", "four", true));
+        Map<String, Object> data2 = new HashMap<>(Map.of("two", "own", "zero", 4, "four", true));
+        Map<String, String> result = App.genDiff(data1, data2);
+        System.out.println(result);
     }
-    public static LinkedHashMap genDiff(Map<String, Object> data1, Map<String, Object> data2) {
-        Map<String, String> result = new LinkedHashMap<>();
-        return (LinkedHashMap) result;
+    public static LinkedHashMap<String, String> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
+        LinkedHashMap<String, String> result = new LinkedHashMap<>();
+        Set<String> united = new HashSet<>(data1.keySet());
+        Set<String> intersection = new HashSet<>(data1.keySet());
+        Set<String> difference = new HashSet<>(data1.keySet());
+        united.addAll(data2.keySet());
+        intersection.retainAll(data2.keySet());
+        difference.removeAll(data2.keySet());
+        for (String s: united) {
+            if (intersection.contains(s)) {
+                result.put(s, data1.get(s).equals(data2.get(s)) ? "unchanged" : "changed");
+            } else {
+                result.put(s, difference.contains(s) ? "deleted" : "added");
+            }
+        }
+        TreeMap<String, String> sorted = new TreeMap<>(result);
+        result.clear();
+        result.putAll(sorted);
+        return result;
     }
 }
 //END

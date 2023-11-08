737f248293de6012ffe1c83ac49301e9508c6f2a
Boris Sturua <stboris65@gmail.com>
2023-11-08 16:16:58 +0300

    restore java-web-ru/ homework

---
+++ java-web-ru/javalin-intro/src/main/java/exercise/App.java
@@ -0,0 +1,20 @@
+package exercise;
+
+// BEGIN
+
+// END
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        // BEGIN
+        
+        // END
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}

ce2838a0837ed6073c47ed03e2f3dac98acfdd3d
@hexlet/cli <support@hexlet.io>
2024-02-08 20:28:15 +0300

    submit java-advanced-ru/multithreading-java

---
+++ java-advanced-ru/multithreading-java/src/main/java/exercise/App.java
@@ -0,0 +1,25 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+class App {
+    private static final Logger LOGGER = Logger.getLogger("AppLogger");
+
+    // BEGIN
+    public static Map<String, Integer> getMinMax(int[] numbers) {
+        HashMap<String, Integer> result = new HashMap<>();
+        var max = new MaxThread(numbers);
+        var min = new MinThread(numbers);
+        max.run();
+        min.run();
+//        max.interrupt();
+//        min.interrupt();
+        result.put("min", min.getMinimum());
+        result.put("max", max.getMaximum());
+        return result;
+    }
+    // END
+}
